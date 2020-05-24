7040054e51075b800be979e98089e56da255e03f
Amir <amirbastidas@gmail.com>
2020-05-24 16:41:51 -0400

    changed the contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,15 +6,20 @@ export default () => {
       <label
         htmlFor="contact-content"
         className="block text-gray-600 text-sm font-bold mb-2"
-      >Tell me your idea to make it true</label>
-      <div className="flex flex-col md:flex-row shadow rounded bg-white border p-2">
+      >Contact me and tell me your idea to make it true</label>
+      <a
+        id="contact-content"
+        href="mailto:amirbastidas@gmail.com"
+        className="text-gray-600 text-xl font-bold"
+      >amirbastidas@gmail.com</a>
+      {/* <div className="flex flex-col md:flex-row shadow rounded bg-white border p-2">
         <textarea
           id="contact-content"
           name="contact-content"
           className="flex-1 py-2 px-3 text-gray-600 focus:outline-none focus:shadow-outline resize-none"
         ></textarea>
         <button className="btn mt-4 md:mt-0 md:ml-4">Enviar</button>
-      </div>
+      </div> */}
     </form>
   );
 }
